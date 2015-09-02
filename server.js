e318402cf8b84577f4d291cecc744bed3471bfe9
Adegoke Obasa <adegoke.obasa@konga.com>
2015-09-02 12:53:33 +0100

    Lesson 6 - Basic web server

--- server.js
+++ server.js
@@ -1,11 +1,46 @@
 var http = require('http'),
-host = '127.0.0.1',
-port = '9000';
+	host = '127.0.0.1',
+	fs = require('fs'),
+	path = require('path'),
+	port = '9000';
+
+// Create a dictionary of mime types
+var mimes = {
+	".html": "text/html",
+	".css": "text/css",
+	".js": "text/javascript",
+	".gif": "image/gif",
+	".jpg": "image/jpeg",
+	".png": "image/png"
+};
 
 var server = http.createServer(function (req, res) {
-	res.writeHead(200, {'Content-Type' : 'text/html'});
-	res.end('<h1>Hello World</h1>');
-}).listen(port, host, function(){
+	var filePath = (req.url === '/') ? ('./index.html') : ('.' + req.url);
+	var contentType = mimes[path.extname(filePath)];
+
+	// Get the real path of the file 
+	var realPath = path.join(__dirname, filePath);
+	console.log(req.url, filePath, realPath);
+	
+	// Check to see if the file exists
+	fs.exists(realPath, function (exists) {
+		if (exists) {
+			// Read and Server
+			fs.readFile(realPath, function (error, content) {
+				if (error) {
+					res.writeHead(500);
+					res.end();
+				} else {
+					res.writeHead(200, {'Content-Type' : contentType});
+					res.end(content, 'utf-8');
+				}
+			});
+		} else {
+			res.writeHead(404);
+			res.end('Sorry we could not find the file you requested !');
+		}
+	});
+}).listen(port, host, function () {
 	console.log("Server Running on http://" + host + ":" + port);
 });
 
