ec63d9cb97ea480cbe8ad6d4d6d742a8a4e074de
Adegoke Obasa <adegoke.obasa@konga.com>
2015-09-02 13:01:40 +0100

    Lesson 7 - Efficient web server

--- server.js
+++ server.js
@@ -20,21 +20,13 @@ var server = http.createServer(function (req, res) {
 
 	// Get the real path of the file 
 	var realPath = path.join(__dirname, filePath);
-	console.log(req.url, filePath, realPath);
 	
 	// Check to see if the file exists
 	fs.exists(realPath, function (exists) {
 		if (exists) {
-			// Read and Server
-			fs.readFile(realPath, function (error, content) {
-				if (error) {
-					res.writeHead(500);
-					res.end();
-				} else {
-					res.writeHead(200, {'Content-Type' : contentType});
-					res.end(content, 'utf-8');
-				}
-			});
+			// Stream file
+			res.writeHead(200, { 'Content-Type': contentType });
+			var streamFile = fs.createReadStream(realPath).pipe(res);
 		} else {
 			res.writeHead(404);
 			res.end('Sorry we could not find the file you requested !');
